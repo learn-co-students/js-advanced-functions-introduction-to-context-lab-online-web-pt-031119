1eae5aeb4d45c17986364f5337e20689f8f2deca
thistonyb <this.tonybaker@gmail.com>
2019-11-08 12:53:09 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Your code here
+function createEmployeeRecord(employeeInfoArray) {
+  let employee = {
+    firstName: employeeInfoArray[0],
+    familyName: employeeInfoArray[1],
+    title: employeeInfoArray[2],
+    payPerHour: employeeInfoArray[3],
+    timeInEvents: [],
+    timeOutEvents: []
+  };
+  return employee;
+}
+
+// function createEmployeeRecords(employeesInfoArray) {
+//   let employeesArray = employeesInfoArray.map(function(employee) {
+//     createEmployeeRecord(employee);
+//   });
+//   return employeesArray;
+// }
+
+function createEmployeeRecords(employeesInfoArray) {
+  let employeesArray = [];
+  employeesInfoArray.forEach(function(employee) {
+    employeesArray.push(createEmployeeRecord(employee));
+  });
+  return employeesArray;
+}
